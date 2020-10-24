3a9f4fce33a18d1314db6621bd3979f06ded8fd1
20041403 <tmtam.c3tqcap.a11@gmail.com>
2020-10-24 23:00:24 +0700

    Xong giao diện màn hình SplashActivity.kt

--- app/src/main/java/vn/vistark/pharmass/MainActivity.kt
+++ app/src/main/java/vn/vistark/pharmass/MainActivity.kt
@@ -1,11 +1,25 @@
 package vn.vistark.pharmass
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import vn.vistark.pharmass.ui.splash.SplashActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Chuyển màn hình sang Splash screen
+        goToSplash()
+    }
+
+    private fun goToSplash() {
+        // Tạo thể hiện chuyển đổi ngữ cảnh sang màn hình mời
+        val intent = Intent(this, SplashActivity::class.java)
+        // Bắt đầu activity mới
+        startActivity(intent)
+        // kết thúc activity hiện tại
+        finish()
     }
 }
