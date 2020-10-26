be92d521691f571b0d7f55601b828694d9726c81
Nhung Van <nhungit.0498@gmail.com>
2020-10-26 22:54:05 +0700

    Hoàn thành tạo màn hình trắng cho WalkThroughScreenActivity.kt

---
+++ app/src/main/java/vn/vistark/pharmass/ui/walkthrough_screen/WalkThroughScreenActivity.kt
@@ -0,0 +1,11 @@
+package vn.vistark.pharmass.ui.walkthrough_screen
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class WalkThroughScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_walk_through_screen)
+    }
+}
